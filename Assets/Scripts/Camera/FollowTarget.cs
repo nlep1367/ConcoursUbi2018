8facada24048f316acd10044a80157a2f430ec9c
Martin Daigle <daim2706@usherbrooke.ca>
2018-01-25 17:05:36 -0500

    Basic Camera / Control system + Navigation Gym

---
+++ Assets/Scripts/Camera/FollowTarget.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowTarget : BaseCamera {
+
+    public float CameraOffsetDistance = 15.0f;
+    public float CameraOffsetAngle = 70;
+
+
+    public float MinFollowSpeed = 1f;
+    public float MaxFollowSpeed = 10f;
+
+    private float CurrentFollowSpeed;
+
+    public Transform TargetToFollow;
+
+
+    private Vector3 ActualTarget;
+    private Vector3 CameraOffset;
+
+    // Use this for initialization
+    void Start() {
+        CameraOffset = new Vector3(0.0f, Mathf.Sin(Mathf.Deg2Rad * CameraOffsetAngle), -Mathf.Cos(Mathf.Deg2Rad * CameraOffsetAngle)) * CameraOffsetDistance;
+
+        ActualTarget = TargetToFollow.position;
+        CurrentFollowSpeed = MinFollowSpeed;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        float Distance = (ActualTarget - TargetToFollow.position).magnitude;
+
+        if (Distance > float.Epsilon)
+        {
+
+            Vector3 Dir = TargetToFollow.position - ActualTarget;
+            Vector3 ImpendingMovement = Dir * Time.deltaTime * CurrentFollowSpeed;
+
+            if (Dir.magnitude < ImpendingMovement.magnitude)
+            {
+                ActualTarget = TargetToFollow.position;
+            }
+            else
+            {
+                ActualTarget += ImpendingMovement;
+                transform.position = ActualTarget + CameraOffset;
+            }
+
+            transform.LookAt(ActualTarget);
+            CurrentFollowSpeed += (MaxFollowSpeed - MinFollowSpeed) * Time.deltaTime / 5;
+
+        }
+        else
+        {
+            CurrentFollowSpeed -= (MaxFollowSpeed - MinFollowSpeed) * Time.deltaTime;
+        }
+
+        CurrentFollowSpeed = Mathf.Clamp(CurrentFollowSpeed, MinFollowSpeed, MaxFollowSpeed);
+    }
+
+    void ResetCamera()
+    {
+        ActualTarget = TargetToFollow.position;
+    }
+}
---
+++ Assets/Scripts/Controls/PlayerMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float Speed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float HorizontalAxis = Input.GetAxis("Horizontal");
+
+        if(HorizontalAxis >= float.Epsilon || HorizontalAxis <= -float.Epsilon)
+        {
+            transform.position += HorizontalAxis * Vector3.right * Speed * Time.deltaTime;
+        }
+
+        float VerticalAxis = Input.GetAxis("Vertical");
+
+        if (VerticalAxis >= float.Epsilon || VerticalAxis <= -float.Epsilon)
+        {
+            transform.position += VerticalAxis * Vector3.forward * Speed * Time.deltaTime;
+        }
+    }
+}
