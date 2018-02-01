f7d6bc26bcf9879df253b416293e961dabc02912
Alexandre Fradette <alexfrad@hotmail.com>
2018-02-01 15:39:09 -0500

    MainMenu kinda working

---
+++ Assets/Scripts/MainMenu/HostGameItem.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class HostGameItem : MonoBehaviour {
+
+    public Button button;
+    public Text gameName;
+
+    private Game game;
+    private HostList hostList;
+
+	// Use this for initialization
+	void Start () {
+        button.onClick.AddListener(HandleClick);
+	}
+	
+	public void SetUp(Game currentGame, HostList currentList)
+    {
+        game = currentGame;
+        gameName.text = game.name;
+
+        hostList = currentList;
+    }
+
+    public void HandleClick()
+    {
+        hostList.AddGame(game);
+    }
+}
