4ac83f7857b220ac85b707b230ace7553ee21723
nlep1367 <lepinenaomi@hotmail.com>
2018-02-12 22:10:44 -0500

    Barre de vie et Tweener

---
+++ Assets/Scripts/UI/LifeBar.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LifeBar : MonoBehaviour {
+
+	public float inValue;
+
+	//public RectTransform mask;
+
+	private int _maxWidth;
+	private float _oldValue;
+
+	private Tweener _tweener;
+
+	// Use this for initialization
+	void Start () {
+		_maxWidth = (int)this.GetComponent<RectTransform> ().sizeDelta [0];
+		_oldValue = 1F;
+
+		_tweener = new Tweener ();
+		_tweener.SetEaseType (Tweener.EaseType.Smooth);
+	}
+
+	void Update() {
+		if (_oldValue != inValue) {
+			_tweener.SetBeginValue (_oldValue);
+			_tweener.SetEndValue (inValue);
+			_tweener.SetDuration (1F);
+
+			_tweener.Start ();
+
+			_oldValue = inValue;
+		}
+
+		if (_tweener.IsActive ()) {
+
+			_tweener.Update ();
+
+			RectTransform mask = transform.Find("mask").GetComponent<RectTransform>();
+			mask.sizeDelta = new Vector2 (_maxWidth * _tweener.GetCurrentValue (), 100);
+		}
+	}
+}
