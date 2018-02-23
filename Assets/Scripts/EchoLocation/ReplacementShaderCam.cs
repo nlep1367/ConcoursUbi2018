c0af190391fc2c0a161a5a6a9dba635174480e3a
Marie-Pier Desharnais <desm3515@usherbrooke.ca>
2018-02-22 19:18:57 -0500

    Base Shaders + Function

---
+++ Assets/Scripts/EchoLocation/ReplacementShaderCam.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[ExecuteInEditMode]
+public class ReplacementShaderCam : MonoBehaviour {
+    public Shader ReplacementShader;
+    void OnEnable()
+    {
+        if (ReplacementShader != null)
+        {
+            GetComponent<Camera>().SetReplacementShader(ReplacementShader, "");
+        }
+    }
+
+    // Update is called once per frame
+    void OnDisable()
+    {
+        GetComponent<Camera>().ResetReplacementShader();
+    }
+}
