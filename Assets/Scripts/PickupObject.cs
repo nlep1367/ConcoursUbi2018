4a5d0b15add06c73c527c3a62e404e68cdef07ea
Sadmonster19 <simon.adam300@gmail.com>
2018-03-05 18:36:55 -0500

    Pickup object

---
+++ Assets/Scripts/PickupObject.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupObject : MonoBehaviour {
+    bool isCarryingObject = false;
+    bool isPickableObjectInRange = false;
+    GameObject pickableObject;
+    GameObject carriedObject;
+    public Transform anchorPoint;
+
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(isCarryingObject)
+        {
+            Carry(carriedObject);
+            Drop();
+        }
+        else
+        {
+            Pickup();
+        }
+	}
+
+    void Carry(GameObject obj)
+    {
+        obj.transform.position = anchorPoint.position;
+        obj.transform.rotation = anchorPoint.rotation;      
+    }
+
+    void Pickup()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            if(isPickableObjectInRange && pickableObject != null)
+            {
+                isCarryingObject = true;
+                carriedObject = pickableObject;
+                carriedObject.GetComponent<Rigidbody>().isKinematic = true;
+            }
+        }
+    }
+
+    void Drop()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            isCarryingObject = false;
+            carriedObject.GetComponent<Rigidbody>().isKinematic = false;
+            carriedObject = null;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("PickableObject"))
+        {
+            isPickableObjectInRange = true;
+            pickableObject = collision.gameObject;
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("PickableObject"))
+        {
+            isPickableObjectInRange = false;
+            pickableObject = null;
+        }
+    }
+}
