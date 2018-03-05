3c246593a268868acc949a518972426cabeedadf
Iangevin <Iannick.Langevin@gmail.com>
2018-03-05 16:21:50 -0500

    Added fear mechanic + danger/safe zone script (#23)
    * Added fear mechanic + danger/safe zone script
    
    * Removev unused array for threshold

---
+++ Assets/Scripts/Fear/Fear.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class Fear : MonoBehaviour {
+
+    public enum FearState {
+        Calm = 15,
+        Anxious = 35,
+        Stress = 60,
+        Panic = 85,
+        NearDeath = 100
+    };
+
+    public FearState fearState = FearState.Calm;
+    public float fearValue = 0.0f;
+   
+    private void UpdateFearState()
+    {
+        foreach (FearState fs in Enum.GetValues(typeof(FearState)))
+        {
+            if (fearValue < (int)fs)
+            {
+                fearState = fs;
+                break;
+            }
+        }
+    }
+
+    public void IncreaseFear(float value)
+    {
+        if (fearValue <= 100)
+        {
+            fearValue = Math.Min(fearValue + value, 100);
+            UpdateFearState();
+        }
+    }
+
+    public void DecreaseFear(float value)
+    {
+        if (fearValue >= 0)
+        {
+            fearValue = Math.Max(fearValue - value, 0);
+            UpdateFearState();
+        }
+    }
+}
