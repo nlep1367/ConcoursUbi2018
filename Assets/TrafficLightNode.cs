ffd23a2e8f1ae6b98233eb05ab719ae3d6494f65
Iannick Langevin <Iannick.langevin@gmail.com>
2018-03-08 15:39:08 -0500

    Added traffic light intersection logic

---
+++ Assets/TrafficLightNode.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrafficLightNode : MonoBehaviour {
+
+
+    private List<TrafficLight> all = new List<TrafficLight>();
+    public List<TrafficLight> horizontal;
+    public List<TrafficLight> vertical;
+
+    // red must equal green + yellow
+    public float redLightDuration = 3f;
+    public float yellowLightDuration = 1f;
+    public float greenLightDuration = 2f;
+
+    public void Start()
+    {
+        foreach (TrafficLight tf in horizontal)
+            tf.lightState = TrafficLightState.Green;
+
+        foreach (TrafficLight tf in vertical)
+            tf.lightState = TrafficLightState.Red;
+
+        all.AddRange(horizontal);
+        all.AddRange(vertical);
+
+        foreach (TrafficLight tf in all)
+            tf.SetLightMaterial();
+    }
+
+    private bool isReadyV = true;
+    private bool isReadyH = true;
+
+    public void Update()
+    {
+        if (isReadyV)
+        {
+            isReadyV = false;
+            ShowNextLightVertical();
+        }
+
+        if (isReadyH)
+        {
+            isReadyH = false;
+            ShowNextLightHorizontal();
+        }
+    }
+
+    void ShowNextLightHorizontal()
+    {
+        if (horizontal.Count == 0)
+            return;
+
+        switch (horizontal[0].lightState)
+        {
+            case TrafficLightState.Green:
+                StartCoroutine(Green(true));
+                break;
+
+            case TrafficLightState.Yellow:
+                StartCoroutine(Yellow(true));
+                break;
+
+            case TrafficLightState.Red:
+                StartCoroutine(Red(true));
+                break;
+        }
+    }
+
+    void ShowNextLightVertical()
+    {
+        if (vertical.Count == 0)
+            return;
+
+        switch (vertical[0].lightState)
+        {
+            case TrafficLightState.Green:
+                StartCoroutine(Green(false));
+                break;
+
+            case TrafficLightState.Yellow:
+                StartCoroutine(Yellow(false));
+                break;
+
+            case TrafficLightState.Red:
+                StartCoroutine(Red(false));
+                break;
+        }
+    }
+
+    public IEnumerator Red(bool isHorizontal)
+    {
+        yield return new WaitForSeconds(redLightDuration);
+        AdjustLight(isHorizontal);
+    }
+
+    public IEnumerator Yellow(bool isHorizontal)
+    {
+        yield return new WaitForSeconds(yellowLightDuration);
+        AdjustLight(isHorizontal);
+    }
+
+    public IEnumerator Green(bool isHorizontal)
+    {
+        yield return new WaitForSeconds(greenLightDuration);
+        AdjustLight(isHorizontal);
+    }
+
+    public void AdjustLight(bool isHorizontal)
+    {
+        if (isHorizontal)
+        {
+            foreach(TrafficLight tf in horizontal)
+            {
+                int nextVal = (int)tf.lightState + 1;
+                tf.lightState = (TrafficLightState)Enum.ToObject(typeof(TrafficLightState), nextVal % (int)TrafficLightState.Count);
+
+                tf.SetLightMaterial();
+            }
+            isReadyH = true;
+        }
+        else
+        {
+            foreach (TrafficLight tf in vertical)
+            {
+                int nextVal = (int)tf.lightState + 1;
+                tf.lightState = (TrafficLightState)Enum.ToObject(typeof(TrafficLightState), nextVal % (int)TrafficLightState.Count);
+                tf.SetLightMaterial();
+            }
+            isReadyV = true;
+        }
+    }
+
+    /*
+    void ShowNextLight()
+    {
+        foreach(TrafficLight tf in all)
+        {
+            switch (tf.lightState)
+            {
+                case TrafficLightState.Green:
+                    StartCoroutine(tf.SetYellow(greenLightDuration));
+                    break;
+
+                case TrafficLightState.Yellow:
+                    StartCoroutine(tf.SetRed(yellowLightDuration));
+                    break;
+
+                case TrafficLightState.Red:
+                    StartCoroutine(tf.SetGreen(redLightDuration));
+                    break;
+            }
+        }
+    }
+    */
+}
