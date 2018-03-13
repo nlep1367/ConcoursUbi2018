8cead62e042c808a8f97f3d6f93658be648f5902
Martin Daigle <martin.s.daigle@usherbrooke.ca>
2018-03-13 08:11:56 -0400

    Added network door

---
+++ Assets/Scripts/Door/DoorState.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Networking;
+using UnityEngine;
+
+public class DoorState : NetworkBehaviour {
+    Animator Anim;
+
+    public bool CloseTest = false;
+    public bool OpenTest = false;
+
+    private bool Opened = false;
+    private bool Idle = false;
+
+    private ObjectSync Os;
+
+    private void Update()
+    {
+        if(OpenTest)
+        {
+            OpenTest = false;
+            OpenDoor();
+        }
+        
+        if(CloseTest)
+        {
+            CloseTest = false;
+            CloseDoor();
+        }
+    }
+
+    void Start()
+    {
+        Anim = GetComponent<Animator>();
+
+        if (!isServer)
+        { 
+            Destroy(Anim);
+            Destroy(this);
+        }
+    }
+
+    public void StopAnimating()
+    {
+        Idle = true;
+    }
+
+    public void OpenDoor()
+    {
+        if (!Idle || Opened)
+            return;
+
+        Anim.Play("Door_Open");
+        Idle = false;
+        Opened = true;
+    }
+
+    public void CloseDoor()
+    {
+        if (!Idle || !Opened)
+            return;
+
+        Anim.Play("Door_Close");
+        Idle = false;
+        Opened = false;
+    }
+}
