d24b5c2753d1c7d1031f97f01a0b83d9396b1bce
Martin Daigle <daim2706@usherbrooke.ca>
2018-03-15 10:42:56 -0400

    Blocker AI start

---
+++ Assets/Scripts/AI/BlockerAI.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockerAI : MonoBehaviour {
+
+    public Transform StartPosition;
+    public Transform EndPosition;
+
+    public AIVision Vision;
+    public IdleBehaviour Idle;
+    
+    // Update is called once per frame
+	void Update ()
+    {
+        Vector3 Target;
+        if (Vision.SeeSomething(out Target))
+        {
+
+        }
+
+	}
+}
