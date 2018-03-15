62c0631abf63e68e9c7b4b33d71821a1afd6e590
Martin Daigle <daim2706@usherbrooke.ca>
2018-03-15 10:09:57 -0400

    QOL changes for the editor, new play button

---
+++ Assets/Editor/StartGame.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+
+public class StartGame : MonoBehaviour {
+
+	[MenuItem("Custom/StartGame")]
+    static void AddAllAi()
+    {
+        EditorSceneManager.OpenScene("Assets/Scenes/MainMenu.unity");
+        EditorApplication.ExecuteMenuItem("Edit/Play");
+    }
+}
