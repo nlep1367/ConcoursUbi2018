fe253b6dd3277a19a42308cdf792b26e985d51ce
Martin Daigle <daim2706@usherbrooke.ca>
2018-03-18 15:48:24 -0400

    AI fonctionnel

--- Assets/Scripts/AI/BlockerAI.cs
+++ Assets/Scripts/AI/BlockerAI.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.AI;
 using UnityEngine;
 
 public class BlockerAI : MonoBehaviour {
@@ -9,16 +10,58 @@ public class BlockerAI : MonoBehaviour {
 
     public AIVision Vision;
     public IdleBehaviour Idle;
-    
+
+    private Vector3 Destination;
+    public float MinDistance = 1;
+
+    public float speed;
+
+    private void Start()
+    {
+    }
+
     // Update is called once per frame
-	void Update ()
+    void Update ()
     {
         Vector3 Target;
+        
         if (Vision.SeeSomething(out Target))
         {
-            Vector3 Path = EndPosition - StartPosition;
-            Mathf 
+            Vector3 Path = EndPosition.position - StartPosition.position;
+            float dot = Vector3.Dot(StartPosition.position - Target, Path);
+
+            if (dot > 1)
+                dot = 1;
+
+            if (dot > 0)
+            {
+                Destination = StartPosition.position + (dot * Path);
+            }
+        }
+        else
+        {
+            Destination = Idle.Process();
         }
 
+        Vector3 Direction = transform.position - Destination;
+
+        if((Direction).magnitude > MinDistance)
+        {
+            Direction.Normalize();
+
+            transform.position += (Direction * speed * Time.deltaTime);
+        }
+
+        transform.LookAt(Target);
 	}
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(StartPosition.position, EndPosition.position);
+
+        Gizmos.color = Color.blue;
+        Gizmos.DrawCube(StartPosition.position, new Vector3(0.5f, 0.5f, 0.5f));
+        Gizmos.DrawCube(EndPosition.position, new Vector3(0.5f, 0.5f, 0.5f));
+    }
 }
---
+++ Assets/Scripts/AI/IdleBehaviour/GotoPoint.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GotoPoint : IdleBehaviour
+{
+    public Vector3 Point;
+
+    public override Vector3 Process()
+    {
+        return Point;
+    }
+}
