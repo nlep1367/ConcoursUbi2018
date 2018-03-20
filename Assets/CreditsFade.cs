73c049b353e783688f04eb9af6454720f214a692
Martin Daigle <daim2706@usherbrooke.ca>
2018-03-20 15:36:17 -0400

    Credit Part 2

--- Assets/CreditsFade.cs
+++ Assets/CreditsFade.cs
@@ -3,55 +3,102 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum FadeState
+{
+    None,
+    FadeIn,
+    Showing,
+    FadeOut
+}
+
 public class CreditsFade : MonoBehaviour {
+
+    public CreditsFade PreviousImage;
 
     public float ShowTime = 2;
 
     public float CurrentTime;
 
-    private bool FadeIn;
-    private bool FadeOut;
+    public FadeState State = FadeState.None;
 
     private bool Showing;
 
-    Action<int> End;
+    Action Ending;
 
     private const float FadeTime = 1;
-	
+    private SpriteRenderer render;
+
+    void Start()
+    {
+        PreviousImage.Ending += BeginAnim;
+        render = GetComponent<SpriteRenderer>();
+
+        if (State == FadeState.FadeIn)
+            Showing = true;
+
+    }
+
+    void BeginAnim()
+    {
+        State = FadeState.FadeIn;
+        Showing = true;
+    }
+
 	// Update is called once per frame
 	void Update () {
+        float CurrentAlpha = 0;
 
+        // If im showing then add time
         if(Showing)
         {
             CurrentTime += Time.deltaTime;
         }
-
-        float CurrentAlpha = 0; ;
-
-        if (FadeIn)
-        { 
-            CurrentAlpha = CurrentTime / FadeTime;
-        }
-        else if(FadeOut)
-        {
-            CurrentAlpha = (ShowTime - CurrentTime) / FadeTime; 
-        }
-
+        
+        // We have ended the sequence
         if(CurrentTime > ShowTime)
         {
             CurrentTime = 0;
             CurrentAlpha = 0;
 
             Showing = false;
-            FadeOut = false;
+            State = FadeState.None;
         }
 
-        if(FadeIn && CurrentAlpha > 1.0f)
-        {
-            FadeIn = false;
-            CurrentAlpha = 1;
-        }
+        // FadeIn is finished
 
+        switch (State)
+        { 
+            case FadeState.None:
+                break;
+
+            case FadeState.FadeIn:
+                CurrentAlpha = CurrentTime / FadeTime;
+
+                if (CurrentAlpha > 1.0f)
+                {
+                    State = FadeState.Showing;
+                    CurrentAlpha = 1;
+                }
+                break;
+
+            case FadeState.Showing:
+                CurrentAlpha = 1;
+
+                if (CurrentTime > ShowTime - FadeTime)
+                {
+                    State = FadeState.FadeOut;
+                    Ending.Invoke();
+                }
+                break;
+
+            case FadeState.FadeOut:
+                CurrentAlpha = (ShowTime - CurrentTime) / FadeTime;
+                
+                break;
+        }
+        
         CurrentAlpha = Mathf.Clamp(CurrentAlpha, 0, 1);
+
+        render.color = new Color(255, 255, 255, CurrentAlpha);
     }
 }
