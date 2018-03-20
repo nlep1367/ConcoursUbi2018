65d8656a9915ab7fef37ffab3745f9d096666fef
Martin Daigle <daim2706@usherbrooke.ca>
2018-03-20 13:36:15 -0400

    Credits Start

---
+++ Assets/CreditsFade.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreditsFade : MonoBehaviour {
+
+    public float ShowTime = 2;
+
+    public float CurrentTime;
+
+    private bool FadeIn;
+    private bool FadeOut;
+
+    private bool Showing;
+
+    Action<int> End;
+
+    private const float FadeTime = 1;
+	
+	// Update is called once per frame
+	void Update () {
+
+        if(Showing)
+        {
+            CurrentTime += Time.deltaTime;
+        }
+
+        float CurrentAlpha = 0; ;
+
+        if (FadeIn)
+        { 
+            CurrentAlpha = CurrentTime / FadeTime;
+        }
+        else if(FadeOut)
+        {
+            CurrentAlpha = (ShowTime - CurrentTime) / FadeTime; 
+        }
+
+        if(CurrentTime > ShowTime)
+        {
+            CurrentTime = 0;
+            CurrentAlpha = 0;
+
+            Showing = false;
+            FadeOut = false;
+        }
+
+        if(FadeIn && CurrentAlpha > 1.0f)
+        {
+            FadeIn = false;
+            CurrentAlpha = 1;
+        }
+
+        CurrentAlpha = Mathf.Clamp(CurrentAlpha, 0, 1);
+    }
+}
