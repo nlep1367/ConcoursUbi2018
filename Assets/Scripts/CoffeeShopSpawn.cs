f5818b3eeceed5082c31bcf9ad6ac841fa53417d
Sadmonster19 <simon.adam300@gmail.com>
2018-03-20 15:06:31 -0400

    First draff

---
+++ Assets/Scripts/CoffeeShopSpawn.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoffeeShopSpawn : MonoBehaviour {
+
+    List<Shop> ShopList;
+
+    GameObject selectedCoffeeShop;
+
+    
+    // Use this for initialization
+	void Start () {
+        ShopList = new List<Shop>();
+	}
+	
+    public void SetShoplist()
+    {
+        var shopsTemp = GameObject.FindObjectsOfType<Shop>();
+
+        foreach(var shop in shopsTemp)
+            ShopList.Add(shop);
+    }
+
+    public void ActivateCoffeeShop()
+    {
+        System.Random rand = new System.Random();
+
+        ShopList[rand.Next(0, ShopList.Count - 1)].isCoffeeShop = true;
+    }
+}
