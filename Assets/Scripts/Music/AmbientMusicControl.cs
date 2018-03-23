4186f9e67d9cc79a4a1b0e55c4a0fddc1db1615d
nlep1367 <lepinenaomi@hotmail.com>
2018-03-23 16:54:31 -0400

    Action sounds added

--- Assets/Scripts/Music/AmbientMusicControl.cs
+++ Assets/Scripts/Music/AmbientMusicControl.cs
@@ -4,23 +4,48 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
 
-public class AmbientMusicControl : MonoBehaviour {
+public class AmbientMusicControl : MonoBehaviour 
+{
+	public enum StingStates 
+	{
+		Start,
+		Win,
+		Fail,
+		Action1,
+		Action2,
+		Action3,
+		Action4
+	}
 
 	public bool ToStartStress = false;
-	public bool ToNextAmbient = false;
 	public bool ToStopStress = false;
+	public bool ToNextAmbient = false;
+	public bool ToWin = false;
+	public bool ToFail = false;
+	public bool ToAction1 = false;
+	public bool ToAction2 = false;
+	public bool ToAction3 = false;
+	public bool ToAction4 = false;
 
 	public AudioMixerSnapshot[] clipTransitions;
 
-	public AudioClip[] ObjectiveSting;
-	public AudioSource stingSource;
+	public AudioClip StartSting;
+	public AudioClip WinSting;
+	public AudioClip FailSting;
+
+	public AudioClip ActionSting1;
+	public AudioClip ActionSting2;
+	public AudioClip ActionSting3;
+	public AudioClip ActionSting4;
 
 	public float bpm = 53;
 
-
 	private GameObject m_AmbientPlayer;
+	private GameObject m_StingPlayer;
 
 	private StressMusicControl m_StressController;
+
+	private AudioSource m_StingSource;
 
 	private AudioSource[] m_AmbientAudioSources;
 	private AudioSource m_NextAmbientAudioSource;
@@ -36,9 +61,13 @@ public class AmbientMusicControl : MonoBehaviour {
 	private bool[] m_AreAmbiantClipsStopping;
 
 	// Use this for initialization
-	void Start () {
+	void Start () 
+	{
 		m_QuarterNote = 60 / bpm;
 		m_Transition = m_QuarterNote * 8;
+
+		m_StingPlayer = transform.Find  ("StingPlayer").gameObject;
+		m_StingSource = m_StingPlayer.GetComponent<AudioSource>();
 
 		m_AmbientPlayer = transform.Find  ("AmbientPlayer").gameObject;
 		m_AmbientAudioSources = m_AmbientPlayer.GetComponents<AudioSource>();
@@ -49,43 +78,31 @@ public class AmbientMusicControl : MonoBehaviour {
 		m_AreAmbiantClipsPlaying = new bool[m_AmbientAudioSources.Length];
 		m_AreAmbiantClipsStopping = new bool[m_AmbientAudioSources.Length];
 
-		for (uint i = 0; i < m_AmbientAudioSources.Length; ++i) {
+		for (uint i = 0; i < m_AmbientAudioSources.Length; ++i) 
+		{
 			m_AreAmbiantClipsPlaying [i] = false;
 			m_AreAmbiantClipsStopping [i] = false;
 		}
 
-		OnPlayAmbient (0);
+		_PlayAmbient (0);
+		PlaySting (StingStates.Start);
 	}
 
 	// Update is called once per frame
 	void Update () 
 	{
-		// Start scared effect
-		if (ToStartStress) {
-			m_StressController.OnPlayStress (m_AmbientAudioClipIndex);
-			ToStartStress = false;
-		}
+		_ReadCommands ();
 
-		// Stop scared effect
-		if (ToStopStress) {
-			m_StressController.OnStopStress (m_AmbientAudioClipIndex);
-			ToStopStress = false;
-		}
-
-		// Go to next ambient clip
-		if (ToNextAmbient) {
-			OnChangeAmbient ();
-			ToNextAmbient = false;
-		}
-			
 		m_StressController.Tick ();
 
-		if (m_IsTransitionning) {
+		if (m_IsTransitionning) 
+		{
 			m_CurrentTime += Time.deltaTime;
 
-			if (m_CurrentTime > m_Transition) {
+			if (m_CurrentTime > m_Transition) 
+			{
 				int index = (m_AmbientAudioClipIndex != 0) ? m_AmbientAudioClipIndex - 1 : m_AmbientAudioSources.Length - 1;
-				OnConcludeAmbiant (index);
+				_ConcludeAmbiant (index);
 			} 
 		}
 	}
@@ -103,30 +120,34 @@ public class AmbientMusicControl : MonoBehaviour {
 			index = ++m_AmbientAudioClipIndex;
 
 		// Play the new ambiant
-		OnPlayAmbient(index);
+		_PlayAmbient(index);
 		clipTransitions [index].TransitionTo (m_Transition);
 
 		// Stop the old ambiant
-		OnStopAmbiant(index != 0 ? index - 1 : tailleMax - 1);
+		_StopAmbiant(index != 0 ? index - 1 : tailleMax - 1);
+
+		PlaySting (StingStates.Start);
 	}
 
-	void OnPlayAmbient(int index)
+	void _PlayAmbient(int index)
 	{	
-		if (!m_AreAmbiantClipsPlaying [index]) {
+		if (!m_AreAmbiantClipsPlaying [index]) 
+		{
 			m_AreAmbiantClipsPlaying [index] = true;
 			m_AmbientAudioSources [index].Play ();
 		}
 	}
 
-	void OnStopAmbiant(int index)
+	void _StopAmbiant(int index)
 	{
-		if (m_AreAmbiantClipsPlaying [index] && !m_AreAmbiantClipsStopping[index]) {
+		if (m_AreAmbiantClipsPlaying [index] && !m_AreAmbiantClipsStopping[index]) 
+		{
 			m_IsTransitionning = true;
 			m_CurrentTime = 0;
 		}
 	}
 
-	void OnConcludeAmbiant(int index)
+	void _ConcludeAmbiant(int index)
 	{
 		m_AreAmbiantClipsPlaying [index] = false;
 		m_AreAmbiantClipsStopping[index] = false;
@@ -135,10 +156,119 @@ public class AmbientMusicControl : MonoBehaviour {
 		m_AmbientAudioSources [index].Stop();
 	}
 
-	void PlaySting()
+	public void PlaySting(StingStates state)
 	{
-		//		int randClip = Random.Range (0, stings.Length);
-		//		stingSource.clip = stings[randClip];
-		//		stingSource.Play();
+		if (!m_StingSource.isPlaying) 
+		{
+			AudioClip clip = _SelectClip (state);
+
+			if (clip != null) 
+			{
+				m_StingSource.clip = _SelectClip(state);
+				m_StingSource.Play();
+			}
+		}
+	}
+
+	AudioClip _SelectClip(StingStates state) 
+	{
+		AudioClip clip;
+
+		switch (state) 
+		{
+		case StingStates.Start: 
+			clip = StartSting;
+			break;
+		case StingStates.Win: 
+			clip = WinSting;
+			break;
+		case StingStates.Fail: 
+			clip = FailSting;
+			break;
+		case StingStates.Action1: 
+			clip = ActionSting1;
+			break;
+		case StingStates.Action2: 
+			clip = ActionSting2;
+			break;
+		case StingStates.Action3: 
+			clip = ActionSting3;
+			break;
+		case StingStates.Action4: 
+			clip = ActionSting4;
+			break;
+		default : 
+			clip = null;
+			break;
+		}
+
+		return clip;
+	}
+
+	void _ReadCommands()
+	{
+		// Start scared effect
+		if (ToStartStress) 
+		{
+			m_StressController.OnPlayStress (m_AmbientAudioClipIndex);
+			ToStartStress = false;
+		}
+
+		// Stop scared effect
+		if (ToStopStress) 
+		{
+			m_StressController.OnStopStress (m_AmbientAudioClipIndex);
+			ToStopStress = false;
+		}
+
+		// Go to next ambient clip
+		if (ToNextAmbient) 
+		{
+			OnChangeAmbient ();
+			ToNextAmbient = false;
+		}
+
+		// Go to next ambient clip
+		if (ToWin) 
+		{
+			PlaySting (StingStates.Win);
+			ToWin = false;
+		}
+
+		// Go to next ambient clip
+		if (ToFail) 
+		{
+			PlaySting (StingStates.Fail);
+			ToFail = false;
+		}
+
+		// Go to next ambient clip
+		if (ToAction1) 
+		{
+			PlaySting (StingStates.Action1);
+			ToAction1 = false;
+		}
+
+		// Go to next ambient clip
+		if (ToAction2) 
+		{
+			PlaySting (StingStates.Action2);
+			ToAction2 = false;
+		}
+
+		// Go to next ambient clip
+		if (ToAction3) 
+		{
+			PlaySting (StingStates.Action3);
+			ToAction3 = false;
+		}
+
+		// Go to next ambient clip
+		if (ToAction4) 
+		{
+			PlaySting (StingStates.Action4);
+			ToAction4 = false;
+		}
+		
 	}
 }
