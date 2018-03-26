09cd0c0e52ffc23400a800ede46bd5bcde80503f
Alexandre <alexfrad@hotmail.com>
2018-03-26 12:13:01 -0400

    Alpha (#72)
    * UI adjustments
    
    * Cooldown UI + started controls manager
    
    * Added game logo, game splash image

--- Assets/Scripts/UI/HintUI.cs
+++ Assets/Scripts/UI/HintUI.cs
@@ -14,27 +14,8 @@ public class HintUI : MonoBehaviour {
     public List<Sprite> sprites;
 
 	public void Display(KeyCode button, string action)
-    {
-        string name = "";
-        switch (button)
-        {
-            case KeyCode.Y:
-                name = "xbox_Y";
-                break;
-            case KeyCode.B:
-                name = "xbox_B";
-                break;
-            case KeyCode.A:
-                name = "xbox_A";
-                break;
-            case KeyCode.X:
-                name = "xbox_X";
-                break;
-            default:
-                name = "xbox_Y";
-                break;
-        }
-        Sprite sprite = FindSprite(name);
+    {        
+        Sprite sprite = FindSprite(button);
 
         if (sprite) buttonImage.sprite = sprite;
         actionTxt.text = action;
@@ -49,10 +30,44 @@ public class HintUI : MonoBehaviour {
         actionTxt.text = null;
     }
 
-    Sprite FindSprite(string name)
+    string FindKeyRef(KeyCode button)
+    {
+        string name = "";
+
+        try
+        {
+            switch (button)
+            {
+                case KeyCode.Y:
+                    name = "xbox_Y";
+                    break;
+                case KeyCode.B:
+                    name = "xbox_B";
+                    break;
+                case KeyCode.A:
+                    name = "xbox_A";
+                    break;
+                case KeyCode.X:
+                    name = "xbox_X";
+                    break;
+                default:
+                    throw new ArgumentException("Hint Panel: There's no references for that keycode");
+            }
+            
+        }
+        catch (ArgumentException e)
+        {
+            Debug.Log(e.Message);
+        }       
+
+        return name;
+    }
+
+    Sprite FindSprite(KeyCode button)
     {
         try
         {
+            string name = FindKeyRef(button);
             if (sprites.Count == 0) throw new ArgumentException("Hint Panel: There's no sprite");
 
             foreach(Sprite img in sprites)
