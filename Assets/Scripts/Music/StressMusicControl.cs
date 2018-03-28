28843b22e293d1c887528ea4abfcb4808043dedd
nlep1367 <lepinenaomi@hotmail.com>
2018-03-28 11:21:05 -0400

    Linked stress music to fear level (waiting for Alex PR to be merged before continuing)

--- Assets/Scripts/Music/StressMusicControl.cs
+++ Assets/Scripts/Music/StressMusicControl.cs
@@ -5,10 +5,12 @@ using UnityEngine.Audio;
 public class StressMusicControl : MonoBehaviour {
 
 	public AudioMixerSnapshot outOfStress;
+	public AudioMixerSnapshot inAnxious;
 	public AudioMixerSnapshot inStress;
+	public AudioMixerSnapshot inPanic;
+	public AudioMixerSnapshot inNearDeath;
 
-	public float TransitionIn;
-	public float TransitionOut;
+	public float TransitionInBetweenStates;
 
 	private GameObject m_StressPlayer;
 
@@ -18,24 +20,47 @@ public class StressMusicControl : MonoBehaviour {
 
 	private bool m_playingStress = false;
 	private bool m_stopping = false;
+	private bool m_isInitialized = false;
+
+	private Fear m_IrisFear;
+	private Fear.FearState m_ActualFearState;
 
 	// Use this for initialization
 	public void Initialize () 
 	{
 		m_StressPlayer = transform.Find  ("StressPlayer").gameObject;
 		m_StressAudioSource = m_StressPlayer.GetComponent<AudioSource>();
+		m_isInitialized = true;
+	}
+
+	void Awake()
+	{
+		if (m_isInitialized) {
+			m_IrisFear = GetComponentInParent<InGameUI>().fille.GetComponent<Fear>();
+			m_ActualFearState = m_IrisFear.fearState;
+			PlayStress(m_ActualFearState);
+		}
 	}
 
 	// Tick is called by the AmbientMusicControl::Update if necessary
 	public void Tick () 
-	{
+	{	
+		if (!m_isInitialized)
+			return;
+		
+		if(m_ActualFearState != m_IrisFear.fearState)
+		{
+			m_ActualFearState = m_IrisFear.fearState;
+			PlayStress(m_ActualFearState);
+		}
+
 		if (!m_playingStress)
 			return;
 
 		if (m_stopping) {
 			m_currentTime += Time.deltaTime;
 
-			if (m_currentTime > TransitionOut) {
+			if (m_currentTime > TransitionInBetweenStates) {
 				OnConcludeStress ();
 			} 
 		}
@@ -46,15 +71,39 @@ public class StressMusicControl : MonoBehaviour {
 		if (!m_playingStress) {
 			m_playingStress = true;
 			m_StressAudioSource.Play();
-			inStress.TransitionTo(TransitionIn);
 		}
+	}
+
+	public void PlayStress(Fear.FearState state)
+	{
+		AudioMixerSnapshot snapToUse;
+		switch (state) {
+		case Fear.FearState.Anxious:
+			snapToUse = inAnxious;
+			break;
+		case Fear.FearState.Stress:
+			snapToUse = inStress;
+			break;
+		case Fear.FearState.Panic:
+			snapToUse = inPanic;
+			break;
+		case Fear.FearState.NearDeath:
+			snapToUse = inNearDeath;
+			break;
+		default:
+		case Fear.FearState.Calm:
+			OnStopStress ();
+			return;
+		}
+		OnPlayStress ();
+		snapToUse.TransitionTo(TransitionInBetweenStates);
 	}
 
 	public void OnStopStress()
 	{
 		if (m_playingStress && !m_stopping) {
 			m_stopping = true;
-			outOfStress.TransitionTo (TransitionOut);
+			outOfStress.TransitionTo (TransitionInBetweenStates);
 			m_currentTime = 0;
 		}
 	}
