f0333fb785f8d250c168f30922d1c847d87cc5f6
nlep1367 <lepinenaomi@hotmail.com>
2018-03-29 00:08:05 -0400

    Forgot a modif

--- Assets/Scripts/Music/EchoSoundsControl.cs
+++ Assets/Scripts/Music/EchoSoundsControl.cs
@@ -6,40 +6,14 @@ public class EchoSoundsControl : CharacterSoundsControl {
 	
 	public AudioClip JoyfulBarkClip;
 	public AudioClip AggressiveBarkClip;
-	public AudioClip PushClip;
-
-	private bool m_IsPushPlaying = false;
 
 	public void BarkJoyfully()
 	{
-		if (!m_IsPushPlaying) {
-			m_AudioSource [1].PlayOneShot (JoyfulBarkClip);
-		}
+		m_AudioSource [1].PlayOneShot (JoyfulBarkClip);
 	}
 
 	public void BarkAggressively()
 	{
-		if (!m_IsPushPlaying) {
-			m_AudioSource [1].PlayOneShot (AggressiveBarkClip);
-		}
-	}
-
-	public void StartPush()
-	{
-		if (m_AudioSource [1].clip != PushClip || !m_IsPushPlaying) {
-			m_IsPushPlaying = true;
-			m_AudioSource [1].loop = true;
-			m_AudioSource [1].clip = PushClip;
-			m_AudioSource [1].Play ();
-		}
-	}
-
-	public void StopPush()
-	{
-		if (m_IsPushPlaying) {
-			m_IsPushPlaying = false;
-			m_AudioSource [1].Stop ();
-			m_AudioSource [1].loop = false;
-		}
+		m_AudioSource [1].PlayOneShot (AggressiveBarkClip);
 	}
 }
